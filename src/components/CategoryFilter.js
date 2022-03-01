f9ad13e21b1f1d74088b79647b7866866edbd312
Bridget Kelly <bckelly197@gmail.com>
2022-03-01 00:29:50 -0500

    complete

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,31 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, onSelectedCategory}) {
+
+  // function handleCategoryClick(event) {
+  //   const clickedButtonElement = event.target;
+  //   const clickedCategory = clickedButtonElement.textContent;
+  //   setSelectedCategory(clickedCategory);
+
+  //   onSelectedCategory(clickedCategory);
+  // }
+  const categoryList = categories.map((category) => {
+    const className = category === selectedCategory ? "selected" : " ";
+    return (
+      <button
+        key={category}
+        className={className}
+        onClick={()=> onSelectedCategory(category)}>
+        {category}
+      </button>
+    )
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryList}
+      
     </div>
   );
 }
